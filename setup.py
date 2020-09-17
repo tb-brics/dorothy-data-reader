26c2fcacef5b8f4b7d35c9c06a307237556a1a82
Paola <paola.plocateli@gmail.com>
2020-09-17 14:42:27 -0300

    Adding the first README.md, adding clinical reader and LICENSE

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="XRayReader",
+    version="0.0.1",
+    author="TB",
+    author_email="author@example.com",
+    description="A x ray metadata reader package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="git@github.com:tb-brics/dorothy-data-reader.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8.5",
+
+    ],
+    python_requires='>=3.6',
+)
